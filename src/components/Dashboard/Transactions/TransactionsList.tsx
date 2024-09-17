c1f66e650a3ffc97ae5ed0187016297e7361de78
Chermann-KING <codes.smiling@gmail.com>
2024-09-17 13:03:00 +0200

    🐛 Main: Components > Dashboard > TransactionsList > Fix flex

--- src/components/Dashboard/Transactions/TransactionsList.tsx
+++ src/components/Dashboard/Transactions/TransactionsList.tsx
@@ -12,7 +12,7 @@ const TransactionsList: React.FC<TransactionsListProps> = ({
   return (
     <>
       {/* transactions list header */}
-      <div className="self-stretch px-4 py-3 justify-start items-center gap-8 flex mt-6">
+      <div className="px-4 py-3 justify-start items-center gap-8 flex mt-6">
         <div className="grow shrink basis-0 text-[#696868] text-xs font-normal font-['Public Sans'] leading-[18px]">
           Recipient / Sender
         </div>
@@ -27,7 +27,7 @@ const TransactionsList: React.FC<TransactionsListProps> = ({
         </div>
       </div>
       {/* transactions list */}
-      <ul className="self-stretch h-[696px] flex-col justify-start items-start flex divide-y divide-grey-100">
+      <ul className="flex-grow flex-col justify-start items-start flex divide-y divide-grey-100">
         {transactions.map((transaction, index) => (
           <TransactionItem key={index} transaction={transaction} />
         ))}
