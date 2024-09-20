23987b3abf277d9be308a31cbb1d18e21cec219e
Chermann-KING <codes.smiling@gmail.com>
2024-09-20 13:34:55 +0200

    ♻️ Main: UI > Generic DeleteConfirmation rendering

--- src/ui/DeleteConfirmationPopup.tsx
+++ src/ui/DeleteConfirmationPopup.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import CloseModalIcon from "@/assets/images/icon-close-modal.svg";
-import Button from "./Button"; // Si tu as déjà un composant Button réutilisable
-import { useBudget } from "@/context/BudgetContext"; // Import du contexte
+import Button from "./Button";
 
 interface DeleteConfirmationProps {
   isOpen: boolean;
   onClose: () => void;
-  budgetCategory: string;
+  itemType: "budget" | "pot"; // Pour spécifier si on supprime un budget ou un pot
+  itemName: string; // Le nom du budget ou du pot
+  onConfirm: () => void; // Fonction pour confirmer la suppression
 }
 
 const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
   isOpen,
   onClose,
-  budgetCategory,
+  itemType,
+  itemName,
+  onConfirm,
 }) => {
-  const { deleteBudget } = useBudget(); // Utilisation du contexte BudgetContext
-
-  const handleDelete = () => {
-    deleteBudget(budgetCategory);
-    onClose(); // Ferme la popup après suppression
-  };
-
   if (!isOpen) return null; // Ne rien afficher si la popup n'est pas ouverte
 
   return (
@@ -30,7 +26,7 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
         {/* header */}
         <div className="flex justify-between items-center">
           <h2 className="text-preset-1 text-grey-900 font-bold">
-            {`Delete '${budgetCategory}'?`}
+            {`Delete '${itemName}'?`}
           </h2>
           <CloseModalIcon
             className="cursor-pointer text-grey-500"
@@ -39,10 +35,10 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
         </div>
         {/* description */}
         <p className="text-grey-500">
-          Are you sure you want to delete this budget? This action cannot be
+          Are you sure you want to delete this {itemType}? This action cannot be
           reversed, and all the data inside it will be removed forever.
         </p>
-        <Button type="button" onClick={handleDelete} variant={"destroy"}>
+        <Button type="button" onClick={onConfirm} variant={"destroy"}>
           Yes, Confirm Deletion
         </Button>
         <button
