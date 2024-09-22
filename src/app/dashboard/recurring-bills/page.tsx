1764b82a539a1096bf62b6cd60f681ec413ee19e
Chermann-KING <codes.smiling@gmail.com>
2024-09-22 02:12:01 +0200

    💄 Main: App > Dashboard > Recurring-Bills > Page updated

--- src/app/dashboard/recurring-bills/page.tsx
+++ src/app/dashboard/recurring-bills/page.tsx
@@ -1,7 +1,57 @@
-export default function RecurrinBillsPage() {
+"use client";
+
+import React from "react";
+import { BillProvider, useBill } from "@/context/BillContext";
+import SearchBar from "@/ui/SearchBar";
+import SortDropdown from "@/ui/SortDropdown";
+import BillsList from "@/components/Dashboard/Bills/BillsList";
+import BillsSummary from "@/components/Dashboard/Bills/BillsSummary";
+import TotalBills from "@/components/Dashboard/Bills/TotalBills";
+
+const RecurringBillsPage = () => {
+  const { bills, setSearchQuery, setSortBy } = useBill();
+
   return (
-    <div className="flex flex-col gap-y-[30px] mx-auto p-6">
-      <h1>Recurrin Bills Page</h1>
+    <div className="flex-grow h-auto px-10 py-9 flex-col justify-start items-center gap-8">
+      {/* title page */}
+      <div className="mb-10 mx-auto w-[1060px] h-14 py-2 justify-start items-center gap-6">
+        <h1 className="text-grey-900 text-preset-1">Recurring Bills</h1>
+      </div>
+      {/* content */}
+      <div className="mx-auto w-[1060px] flex justify-between gap-x-6">
+        {/* total bills & summary */}
+        <div className="w-[337px] h-[418px] flex flex-col gap-y-6">
+          <TotalBills />
+          <BillsSummary />
+        </div>
+
+        {/* bills list */}
+        <div className="w-[669px] bg-white flex flex-col gap-y-6 p-8 rounded-xl">
+          {/* search & sort by */}
+          <div className="flex justify-between items-center ">
+            <div className="w-[320px]">
+              <SearchBar
+                setSearchQuery={setSearchQuery}
+                placeholderText="Search bills"
+              />
+            </div>
+            {/* sort by */}
+            <div className="w-[169px] flex justify-between items-center">
+              <span className="text-grey-500 text-preset-4">Sort by</span>
+              <SortDropdown setSortBy={setSortBy} setCurrentPage={() => {}} />
+            </div>
+          </div>
+          <BillsList bills={bills} />
+        </div>
+      </div>
     </div>
+  );
+};
+
+export default function Page() {
+  return (
+    <BillProvider>
+      <RecurringBillsPage />
+    </BillProvider>
   );
 }
