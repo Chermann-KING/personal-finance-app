df507b6caeb7dfce7101844799d54fdf6a7e5000
Chermann-KING <codes.smiling@gmail.com>
2024-09-26 19:00:21 +0200

    🐛 Main: UI > SearchBar > Fix the display bug to CategoriesDropdown & SortDropdown

--- src/ui/CategoriesDropdown.tsx
+++ src/ui/CategoriesDropdown.tsx
@@ -23,7 +23,7 @@ interface CategoryDropdownProps {
 
 const CategoriesDropdown: React.FC<CategoryDropdownProps> = ({
   label,
-  initialSelectedOption = "All Transactions", // Défaut à "All Transactions"
+  initialSelectedOption = "All Transactions",
   onOptionChange,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -64,75 +64,69 @@ const CategoriesDropdown: React.FC<CategoryDropdownProps> = ({
   }, []);
 
   return (
-    <div className="justify-start items-center gap-2 flex">
+    <div
+      className="max-w-[245px] relative flex justify-end items-center gap-x-2"
+      ref={dropdownRef}
+    >
       {/* label visible uniquement sur les écrans sm (>= 640px) */}
       {label && (
         <label className="hidden sm:block text-grey-500 text-preset-4">
           Category
         </label>
       )}
-
-      <div className="flex-col justify-start items-start gap-1 inline-flex">
-        <div className="relative w-full min-w-56" ref={dropdownRef}>
-          {/* Bouton pour le tri visible sur mobile uniquement */}
-          <button
-            type="button"
-            onClick={toggleDropdown}
-            className="sm:hidden flex items-center justify-center p-2 focus:border-gray-900 focus:outline-none"
-          >
-            <FilterMobileIcon />
-          </button>
-
-          {/* Bouton d'ouverture du dropdown visible uniquement sur les écrans sm (>= 640px) */}
-          <button
-            type="button"
-            onClick={toggleDropdown}
-            className="hidden sm:inline-flex justify-between items-center w-full bg-white border border-gray-300 focus:border-gray-900 focus:outline-none text-[0.875rem] font-normal text-gray-900 px-[19px] py-[14px] rounded-lg"
-          >
-            <span>{selectedOption}</span>
-            <CaretDownIcon
-              className={`transform ${isOpen ? "rotate-180" : ""}`}
-            />
-          </button>
-
-          {/* Dropdown menu visible après clic sur l'icône de filtre (mobile) ou bouton (desktop) */}
+      {/* Bouton pour le tri visible sur mobile uniquement */}
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        className="sm:hidden flex items-center justify-center p-2 focus:border-gray-900 focus:outline-none text-sm"
+      >
+        <FilterMobileIcon />
+      </button>
+      {/* Bouton d'ouverture du dropdown visible uniquement sur les écrans sm (>= 640px) */}
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        className="hidden sm:inline-flex justify-between items-center w-[177px] bg-white border border-gray-300 focus:border-gray-900 focus:outline-none text-[0.875rem] font-normal text-gray-900 px-[19px] py-[14px] rounded-lg"
+      >
+        <span>{selectedOption}</span>
+        <CaretDownIcon className={`transform ${isOpen ? "rotate-180" : ""}`} />
+      </button>
+      {/* Dropdown menu visible après clic sur l'icône de filtre (mobile) ou bouton (desktop) */}
+      <div
+        role="listbox"
+        className={`h-auto max-h-[333px] overflow-y-scroll scrollbar-thin no-scrollbar absolute sm:right-0 w-full sm:mt-2 rounded-lg shadow-custom bg-white z-10 divide-y divide-solid divide-grey-100 px-[19px] transform transition-all duration-300 ease-in-out ${
+          isOpen
+            ? "opacity-100 translate-y-0 visible"
+            : "opacity-0 -translate-y-2 invisible"
+        } w-[177px] right-0 top-11 sm:top-[50px]`}
+      >
+        {(
+          [
+            "All Transactions",
+            "Entertainment",
+            "Bills",
+            "Groceries",
+            "Dining Out",
+            "Transportation",
+            "Personal Care",
+            "Education",
+            "Lifestyle",
+            "Shopping",
+            "General",
+          ] as CategoryDropdownOptions[]
+        ).map((option) => (
           <div
-            role="listbox"
-            className={`h-auto max-h-[333px] overflow-y-scroll scrollbar-thin no-scrollbar absolute right-0 w-full mt-2 rounded-lg shadow-custom bg-white z-10 divide-y divide-solid divide-grey-100 px-[19px] transform transition-all duration-300 ease-in-out ${
-              isOpen
-                ? "opacity-100 translate-y-0 visible"
-                : "opacity-0 -translate-y-4 invisible"
+            key={option}
+            className={`cursor-pointer py-[14px] text-[0.9375rem] text-gray-700 transition-colors duration-200 ${
+              selectedOption === option ? " font-bold" : "hover:font-bold"
             }`}
+            onClick={(e) => handleOptionChange(option, e)}
+            role="option"
+            aria-selected={selectedOption === option}
           >
-            {(
-              [
-                "All Transactions",
-                "Entertainment",
-                "Bills",
-                "Groceries",
-                "Dining Out",
-                "Transportation",
-                "Personal Care",
-                "Education",
-                "Lifestyle",
-                "Shopping",
-                "General",
-              ] as CategoryDropdownOptions[]
-            ).map((option) => (
-              <div
-                key={option}
-                className={`cursor-pointer  py-[14px] text-[0.9375rem] text-gray-700  transition-colors duration-200 ${
-                  selectedOption === option ? " font-bold" : "hover:font-bold"
-                }`}
-                onClick={(e) => handleOptionChange(option, e)}
-                role="option"
-                aria-selected={selectedOption === option}
-              >
-                {option}
-              </div>
-            ))}
+            {option}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/ui/SortDropdown.tsx
+++ src/ui/SortDropdown.tsx
@@ -57,72 +57,64 @@ const SortDropdown: React.FC<SortDropdownProps> = ({
   }, []);
 
   return (
-    <div className="justify-start items-center gap-2 flex">
+    <div
+      className="max-w-[172px] relative flex justify-between items-center"
+      ref={dropdownRef}
+    >
       {/* label visible uniquement sur les écrans sm (>= 640px) */}
       {label && (
-        <label className="hidden sm:block text-grey-500 text-preset-4">
+        <label className="hidden sm:block text-grey-500 text-preset-4 mr-2 text-nowrap">
           Sort by
         </label>
       )}
-
-      <div className="flex-col justify-start items-start gap-1 inline-flex">
-        <div className="relative w-[113px]" ref={dropdownRef}>
-          {/* Bouton pour le tri visible sur mobile uniquement */}
-          <button
-            type="button"
-            onClick={toggleDropdown}
-            className="sm:hidden flex items-center justify-center p-2 focus:border-gray-900 focus:outline-none"
-          >
-            <SortMobileIcon />
-          </button>
-
-          {/* Bouton d'ouverture du dropdown visible uniquement sur les écrans sm (>= 640px) */}
-          <button
-            type="button"
-            onClick={toggleDropdown}
-            className="hidden sm:inline-flex justify-between items-center w-full bg-white border border-gray-300 focus:border-gray-900 focus:outline-none text-[0.875rem] font-normal text-gray-900 px-[19px] py-[14px] rounded-lg"
-          >
-            <span>{selectedOption}</span>
-            <CaretDownIcon
-              className={`transform ${isOpen ? "rotate-180" : ""}`}
-            />
-          </button>
-
-          {/* Dropdown menu visible après clic sur l'icône de tri (mobile) ou bouton (desktop) */}
+      {/* Bouton pour le tri visible sur mobile uniquement */}
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        className="sm:hidden flex items-center justify-center p-2 focus:border-gray-900 focus:outline-none"
+      >
+        <SortMobileIcon />
+      </button>
+      {/* Bouton d'ouverture du dropdown visible uniquement sur les écrans sm (>= 640px) */}
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        className="hidden sm:inline-flex justify-between items-center w-[113px] bg-white border border-gray-300 focus:border-gray-900 focus:outline-none text-[0.875rem] font-normal text-gray-900 px-[19px] py-[14px] rounded-lg"
+      >
+        <span>{selectedOption}</span>
+        <CaretDownIcon className={`transform ${isOpen ? "rotate-180" : ""}`} />
+      </button>
+      {/* Dropdown menu visible après clic sur l'icône de tri (mobile) ou bouton (desktop) */}
+      <div
+        role="listbox"
+        className={`h-auto max-h-[333px] overflow-y-scroll scrollbar-thin no-scrollbar absolute right-0 w-full mt-2 rounded-lg shadow-custom bg-white z-10 divide-y divide-solid divide-grey-100 px-[19px] transform transition-all duration-300 ease-in-out ${
+          isOpen
+            ? "opacity-100 translate-y-0 visible"
+            : "opacity-0 -translate-y-2 invisible"
+        } w-[114px] right-0 top-9 sm:top-[50px]`}
+      >
+        {[
+          { label: "Latest", value: "Latest" },
+          { label: "Oldest", value: "Oldest" },
+          { label: "A to Z", value: "A to Z" },
+          { label: "Z to A", value: "Z to A" },
+          { label: "Highest", value: "Highest" },
+          { label: "Lowest", value: "Lowest" },
+        ].map((option) => (
           <div
-            role="listbox"
-            className={`h-auto max-h-[333px] overflow-y-scroll scrollbar-thin no-scrollbar absolute right-0 w-full mt-2 rounded-lg shadow-custom bg-white z-10 divide-y divide-solid divide-grey-100 px-[19px] transform transition-all duration-300 ease-in-out ${
-              isOpen
-                ? "opacity-100 translate-y-0 visible"
-                : "opacity-0 -translate-y-4 invisible"
+            key={option.value}
+            className={`cursor-pointer py-[14px] text-[0.9375rem] text-gray-700 transition-colors duration-200 ${
+              selectedOption === option.label ? "font-bold" : "hover:font-bold"
             }`}
+            onClick={(e) =>
+              handleOptionChange(option.value as SortDropdownOptions, e)
+            }
+            role="option"
+            aria-selected={selectedOption === option.label}
           >
-            {[
-              { label: "Latest", value: "Latest" },
-              { label: "Oldest", value: "Oldest" },
-              { label: "A to Z", value: "A to Z" },
-              { label: "Z to A", value: "Z to A" },
-              { label: "Highest", value: "Highest" },
-              { label: "Lowest", value: "Lowest" },
-            ].map((option) => (
-              <div
-                key={option.value}
-                className={`cursor-pointer py-[14px] text-[0.9375rem] text-gray-700 transition-colors duration-200 ${
-                  selectedOption === option.label
-                    ? "font-bold"
-                    : "hover:font-bold"
-                }`}
-                onClick={(e) =>
-                  handleOptionChange(option.value as SortDropdownOptions, e)
-                }
-                role="option"
-                aria-selected={selectedOption === option.label}
-              >
-                {option.label}
-              </div>
-            ))}
+            {option.label}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
