2a901e592262a12c4d7d1e2ec7257eeaac1e56e7
Chermann-KING <codes.smiling@gmail.com>
2024-10-01 18:07:25 +0200

    💄 Main: App > Dashboard > Layout > Update padding

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
       <Sidebar />
       {/* RIGHT */}
       <div className="w-full overflow-y-scroll flex flex-col items-center px-4 py-6 md:px-10 md:py-8">
-        <div className="max-w-[1060px]">{children}</div>
+        <div className="max-w-[1060px] pb-12 sm:pb-0">{children}</div>
       </div>
     </div>
   );
