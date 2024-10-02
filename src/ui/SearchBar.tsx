15bea4fcf994233edd8680234fb52cbef33a2eb2
Chermann-KING <codes.smiling@gmail.com>
2024-10-02 21:34:49 +0200

    📱 Main: App > UI > SerachBar > Fix the width

--- src/ui/SearchBar.tsx
+++ src/ui/SearchBar.tsx
@@ -31,13 +31,13 @@ export default function SearchBar({
   placeholderText = "Search...",
 }: SearchBarProps) {
   return (
-    <div className="w-[215px] md:w-[320px] sm:w-[162px] relative flex bg-white rounded-lg">
+    <div className="w-full md:min-w-[162px] lg:max-w-[320px] relative flex bg-white rounded-lg">
       {/* Champ de saisie pour la recherche */}
       <input
         type="text"
         name="search"
         placeholder={placeholderText}
-        className={`block w-full min-w-[215px] rounded-md border-0 py-3.5 px-5 pr-10 md:pr-10 sm:pr-10 text-grey-900 ring-1 ring-inset ring-grey-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-black sm:text-preset-5`}
+        className={`block w-full rounded-md border-0 py-3.5 px-5 pr-10 md:pr-10 sm:pr-10 text-grey-900 ring-1 ring-inset ring-grey-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-black sm:text-preset-5`}
         style={placeholderStyle}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
